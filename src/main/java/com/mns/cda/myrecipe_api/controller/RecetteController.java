423357ca7411144b2d79747554f4bb950e0397dc
Dorian Weber <dorianweb57@gmail.com>
2026-04-27 11:47:01 +0200

    "Ajout de l'entity Recette avec model"

---
+++ src/main/java/com/mns/cda/myrecipe_api/controller/RecetteController.java
@@ -0,0 +1,11 @@
+package com.mns.cda.myrecipe_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recette")
+public class RecetteController {
+}
--- src/main/java/com/mns/cda/myrecipe_api/model/Recette.java
+++ src/main/java/com/mns/cda/myrecipe_api/model/Recette.java
@@ -1,5 +1,44 @@
 package com.mns.cda.myrecipe_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Recette {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String titreRecette;
+
+    private String nomPlat;
+
+    private String descriptionRecette;
+
+    private int tempPreparationMinute;
+
+    @CreatedDate
+    private LocalDateTime dateRecette;
+
+    @LastModifiedDate
+    private LocalDateTime dateRecetteModifier;
+
+
+
 
 }
