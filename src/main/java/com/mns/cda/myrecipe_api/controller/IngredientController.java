2491e4def030d1a23e6d4d002024cb770926b262
Ludvinae <benoit.champlon79@gmail.com>
2026-04-28 09:47:57 +0200

    feat: ajout du controller ingrédientÃ

--- src/main/java/com/mns/cda/myrecipe_api/controller/IngredientController.java
+++ src/main/java/com/mns/cda/myrecipe_api/controller/IngredientController.java
@@ -2,20 +2,23 @@ package com.mns.cda.myrecipe_api.controller;
 
 import com.mns.cda.myrecipe_api.dao.IngredientDao;
 import com.mns.cda.myrecipe_api.model.Ingredient;
+import com.mns.cda.myrecipe_api.model.Ingredient;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/ingredients")
+@RequestMapping("/ingredient")
 @CrossOrigin
 @RequiredArgsConstructor
 @Tag(name="Ingrédients", description ="Gestion des ingrédients possibles")
@@ -25,10 +28,11 @@ public class IngredientController {
 
     @Operation(summary="Récupérer tous les ingrédients")
     @ApiResponse(responseCode= "200", description="Liste des ingrédients")
-    @GetMapping
+    @GetMapping("/liste")
     public List<Ingredient> getAllIngredients() {
         return ingredientDao.findAll();
     }
+
 
     @Operation(summary="Récupérer un ingrédient")
     @ApiResponses({
@@ -43,5 +47,60 @@ public class IngredientController {
         }
 
         return new ResponseEntity<>(ingredient.get(), HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "Crée un ingrédient")
+    @ApiResponses({@ApiResponse(responseCode = "201", description = "Ingrédient crée"),
+            @ApiResponse(responseCode = "400", description = "Données invalides")})
+    @PostMapping
+    public ResponseEntity<Ingredient> create(@RequestBody @Valid Ingredient ingredient) {
+        Ingredient ingredientSaved = ingredientDao.save(ingredient);
+
+        return new ResponseEntity<>(ingredientSaved, HttpStatus.CREATED);
+    }
+
+
+    @Operation(
+            summary = "Supprimer un ingrédient",
+            description = "Supprime un ingrédient existant")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Ingrédient supprimé"),
+            @ApiResponse(responseCode = "404", description = "Ingrédient non trouvé")})
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        Optional<Ingredient> ingredient = ingredientDao.findById(id);
+        if (ingredient.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        ingredientDao.delete(ingredient.get());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @Operation(
+            summary = "Mettre à jour un logiciel",
+            description = "Met à jour les informations d’un logiciel ('name', 'description', 'ingredientType')")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Logiciel mis à jour"),
+            @ApiResponse(responseCode = "404", description = "Logiciel non trouvé"),
+            @ApiResponse(responseCode = "400", description = "Données invalides")})
+    @PutMapping("/{id}")
+    public ResponseEntity<Ingredient> update(
+            @PathVariable Integer id,
+            @RequestBody Ingredient ingredientToUpdate) {
+
+        Optional<Ingredient> optionalIngredient = ingredientDao.findById(id);
+
+        if(optionalIngredient.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        ingredientToUpdate.setId(id);
+
+        ingredientDao.save(ingredientToUpdate);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
 }
